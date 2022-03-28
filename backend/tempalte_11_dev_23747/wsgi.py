0820390db63454cc1602939b9cd3ec967137f0b4
Crowdbotics <engineering@crowdbotics.com>
2022-03-28 05:26:36 +0000

    Initial commit.

---
+++ backend/tempalte_11_dev_23747/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tempalte_11_dev_23747 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tempalte_11_dev_23747.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tempalte_11_dev_23747",
+  url: "https://tempalte_11_dev_23747.botics.co",
+  api: "https://tempalte_11_dev_23747.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
